b0589f71e09bec429d7438e3f25896f3eba62ae6
Andre Vitor <andrevtr29@gmail.com>
2023-11-24 21:51:42 -0300

    2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/',(request,response)=>{
+app.get('/',(request,response) => {
     return response.json({message:'Server is Up'});
 })
 
