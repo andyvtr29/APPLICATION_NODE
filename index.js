b977d4fc4a2d6ccb115d96ee2d6231d161e87dc9
Andre Vitor <andrevtr29@gmail.com>
2023-11-26 11:22:15 -0300

    CI Adicionando outra rota

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
 
-app.get('/',(request,response) => {
-    return response.json({message:'Server is Up'});
-})
+app.get('/', (request, response) => {
+  return response.json({ message: 'Server is Up' });
+});
 
+app.get('/outra-rota', (request, response) => {
+  return response.json({ message: 'Atualizou mesmo!' });
+});
 
-app.get('/',(request,response) => {
-    return response.json({message:'Atualizou mesmo!'});
-    
-})
-app.post('/teste',(request,response) => {
-    const {name,date} = request.body;
-    
-    return response.json({name,date});
+app.post('/teste', (request, response) => {
+  const { name, date } = request.body;
+  return response.json({ name, date });
+});
 
-})
-
-app.listen(3333)
+app.listen(3333, () => {
+  console.log('Servidor rodando na porta 3333');
+});
